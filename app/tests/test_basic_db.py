143d95402d14472f43b1f7c6ad3e4598e3e95a6c
sferreyra <ferreyrasergio@gmail.com>
2018-12-28 15:23:50 -0300

    test percent

--- app/tests/test_basic_db.py
+++ app/tests/test_basic_db.py
@@ -1,78 +1,10 @@
-import sqlalchemy
-from app import app
-from redis import Redis
-from flask import Flask, render_template, request
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-import os
-import unittest
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
- 
-
-database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['PG_USER'],
-    dbpass=os.environ['PG_PASS'],
-    dbhost=os.environ['PG_HOST'],
-    dbname=os.environ['PG_DB']
-)
-
-app = Flask(__name__)
-app.config.update(
-    SQLALCHEMY_DATABASE_URI=database_uri,
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
-)
+# pip install pytest requests
+# py.test
+import requests
 
 
-# initialize the database connection
-db = SQLAlchemy(app)
-
-# initialize database migration management
-migrate = Migrate(app, db)
- 
- 
- 
-class BasicTests(unittest.TestCase):
- 
-    ############################
-    #### setup and teardown ####
-    ############################
- 
-    # executed prior to each test
-    def setUp(self):
-        global transaction, connection, engine
-       
-        # Connect to the database and create the schema within a transaction
-        engine = create_engine('postgresql+psycopg2://username:secretpassword@demodb-postgresql.jx.svc.cluster.local/my-database')
-#        connection = engine.connect()
-#        transaction = connection.begin()
-#        Base.metadata.create_all(connection)
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] 
-        self.app = app.test_client()
-        db.drop_all()
-        db.create_all()
- 
-        # Disable sending emails during unit testing
-        #mail.init_app(app)
-        #self.assertEqual(app.debug, False)
- 
-    # executed after each test
-    def tearDown(self):
-        pass
- 
- 
-###############
-#### tests ####
-###############
- 
-#    def test_main_page(self):
-#        response = self.app.get('/', follow_redirects=True)
-#        self.assertEqual(response.status_code, 200)
- 
- 
-if __name__ == "__main__":
-    unittest.main()
+def test_home():
+    "GET request to url returns a 200"
+    url = 'http://demo-mockapp.jx-staging.flugel.it/'
+    resp = requests.get(url)
+    assert resp.status_code == 200
