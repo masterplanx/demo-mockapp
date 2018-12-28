7a4e4d965a7464e0def42d0f3a71d98593d4afcc
sferreyra <ferreyrasergio@gmail.com>
2018-12-28 13:13:51 -0300

    test db

--- app/tests/test_basic.py
+++ app/tests/test_basic.py
@@ -1,10 +1,49 @@
-# pip install pytest requests
-# py.test
-import requests
+from app import app
+import os
+import sys
+import unittest
+import pytest
+from redis import Redis
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import Session
 
 
-def test_home():
-    "GET request to url returns a 200"
-    url = 'http://demo-mockapp.jx-staging.flugel.it/colin7070'
-    resp = requests.get(url)
-    assert resp.status_code == 200
+
+
+database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.environ['PG_USER'],
+    dbpass=os.environ['PG_PASS'],
+    dbhost=os.environ['PG_HOST'],
+    dbname=os.environ['PG_DB']
+)
+
+
+def setup_module():
+    global transaction, connection, engine
+
+    # Connect to the database and create the schema within a transaction
+    engine = create_engine(database_uri)
+    connection = engine.connect()
+    transaction = connection.begin()
+    Base.metadata.create_all(connection)
+
+    # If you want to insert fixtures to the DB, do it here
+
+
+def teardown_module():
+    # Roll back the top level transaction and disconnect from the database
+    transaction.rollback()
+    connection.close()
+    engine.dispose()
+ 
+
+class DatabaseTest(object):
+    def setup(self):
+        self.__transaction = connection.begin_nested()
+        self.session = Session(connection)
+
+    def teardown(self):
+        self.session.close()
+        self.__transaction.rollback()
