ffb8c22e9b06a8100139ae4464639970d2530c88
sferreyra <ferreyrasergio@gmail.com>
2018-12-28 13:02:46 -0300

    Failing test

--- app/tests/test_basic.py
+++ app/tests/test_basic.py
@@ -5,6 +5,6 @@ import requests
 
 def test_home():
     "GET request to url returns a 200"
-    url = 'http://demo-mockapp.jx-staging.flugel.it/'
+    url = 'http://demo-mockapp.jx-staging.flugel.it/colin7070'
     resp = requests.get(url)
     assert resp.status_code == 200
