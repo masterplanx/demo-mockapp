7c792f264e951914a0a59f89f48d556d4b013f0a
sferreyra <ferreyrasergio@gmail.com>
2018-12-28 12:34:37 -0300

    get cache

--- app/tests/test_basic.py
+++ app/tests/test_basic.py
@@ -23,7 +23,7 @@ class FlaskBookshelfTests(unittest.TestCase):
     def test_home_status_code(self):
         # sends HTTP GET request to the application
         # on the specified path
-        result = self.app.get('/') 
+        result = self.app.get('/cache') 
 
         # assert the status code of the response
         self.assertEqual(result.status_code, 200) 
