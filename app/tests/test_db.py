f6bf26926a762ed1f3584f71fb771ecdcfe37ea4
sferreyra <ferreyrasergio@gmail.com>
2018-12-28 17:28:11 -0300

    test

--- app/tests/test_db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Table, MetaData, create_engine
-engine = create_engine('postgresql+psycopg2://username:secretpassword@demodb-postgresql.jx.svc.cluster.local/my-database')
-with engine.connect() as conn:
-  conn.execute("SELECT VERSION()")
-  meta = MetaData()
-  referring = Table('guests', meta,
-  autoload=True, autoload_with=conn)
